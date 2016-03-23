80ea93a16e5a3fb42ce230d70f104fbfb435ec58
wengjiaojiao <419909058@qq.com>
2016-03-23 17:22:47 +0800

    Update practice_1_spec.js

--- specs/section_1/practice_1_spec.js
+++ specs/section_1/practice_1_spec.js
@@ -7,7 +7,7 @@ describe('practice-1-1', function() {
 
   it("选出A集合中与B集合中相同的元素", function() {
     var result = collect_same_elements(collection_a, collection_b);
-
+    console.log(result);
     expect(result).toEqual(["a", "e", "f", "d"]);
   });
 });
